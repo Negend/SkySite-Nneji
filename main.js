77cee2f2f7356e387408e78125c32c2b6046801a
Nneji Nwakamma <nneji.2day@yahoo.co.uk>
2019-03-10 16:01:53 +0000

    upload page css and html

--- main.js
+++ main.js
@@ -102,9 +102,10 @@ function loaded (){
 	  var xhttp = new XMLHttpRequest();
 	  xhttp.open("GET", file, true)  
 	  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    getProgramFromFile(this);
-    }
+	    if (this.readyState == 4 && this.status == 200) {
+	    	getProgramFromFile(this);
+	    }
+	  }  
 	  xhttp.send()
 	}
 
